3fe9cd351109a029e7bc6f1b70a725971eec417a
Hemal Pansuriya <hemalpansuriya007@gmail.com>
2021-06-22 19:31:22 +0530

    Adding more contents and features

---
+++ pk/js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    $('#btnpopover').popover();
+    // $("input[type='search']").focus();
+});
+$(document).ready(function () {
+    $('.1i').hover(function () {
+        $('.1qw').toggle();
+        $('.1w').toggle();
+    });
+    $('.2i').hover(function () {
+        $('.2qw').toggle();
+        $('.2w').toggle();
+    });
+    $('.3i').hover(function () {
+        $('.3qw').toggle();
+        $('.3w').toggle();
+    });
+    $('.4i').hover(function () {
+        $('.4qw').toggle();
+        $('.4w').toggle();
+    });
+    $('.5i').hover(function () {
+        $('.5qw').toggle();
+        $('.5w').toggle();
+    });
+    $('.6i').hover(function () {
+        $('.6qw').toggle();
+        $('.6w').toggle();
+    });
+    $('.7i').hover(function () {
+        $('.7qw').toggle();
+        $('.7w').toggle();
+    });
+    $('.8i').hover(function () {
+        $('.8qw').toggle();
+        $('.8w').toggle();
+    });
+    $('.9i').hover(function () {
+        $('.9qw').toggle();
+        $('.9w').toggle();
+    });
+    $('.10i').hover(function () {
+        $('.10qw').toggle();
+        $('.10w').toggle();
+    });
+    $('.11i').hover(function () {
+        $('.11qw').toggle();
+        $('.11w').toggle();
+    });
+    $('.12i').hover(function () {
+        $('.12qw').toggle();
+        $('.12w').toggle();
+    });
+    $('.13i').hover(function () {
+        $('.13qw').toggle();
+        $('.13w').toggle();
+    });
+    $('.14i').hover(function () {
+        $('.14qw').toggle();
+        $('.14w').toggle();
+    });
+    $('.15i').hover(function () {
+        $('.15qw').toggle();
+        $('.15w').toggle();
+    });
+    $('.16i').hover(function () {
+        $('.16qw').toggle();
+        $('.16w').toggle();
+    });
+});
